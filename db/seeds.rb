0b4141450d10c1a176fd341869cf6463183aefba
Mistah-Jae <160692244+Mistah-Jae@users.noreply.github.com>
2024-06-06 15:44:48 +0800

    Create tasks seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,34 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "open-uri"
+
+require 'date'
+
+puts 'creating new users'
+
+alexis = User.create!(email: 'alexis@test.com', password: '123456')
+
+puts 'creating new taks'
+
+new_tasks = [
+  { title: 'Finish reading and replying emails',
+    description: 'Double check staff overtime and expense claims',
+    priority: 'High',
+    due_date: Date.today,
+    reminder_date_time:, DateTime.now + 30.minutes,
+    user: alexis
+    },
+
+  { title: ''
+
+  }
+
+
+
+
+
+
+
+
+]
