ba0c89147d57b6dbf3eb6c0a2d8be254bf3cadb3
Mistah-Jae <160692244+Mistah-Jae@users.noreply.github.com>
2024-06-06 16:31:27 +0800

    Created task seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,34 +7,60 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-require "open-uri"
-
+# require "open-uri" - (Allows us to open URL)
 require 'date'
+
+puts 'Destroying database'
+Task.destroy_all
+User.destroy_all
 
 puts 'creating new users'
 
-alexis = User.create!(email: 'alexis@test.com', password: '123456')
+alexis = User.create!(email: 'alexis@test.com', password: '123abc')
 
-puts 'creating new taks'
+puts 'creating new tasks'
 
 new_tasks = [
   { title: 'Finish reading and replying emails',
     description: 'Double check staff overtime and expense claims',
-    priority: 'High',
+    priority: 'high',
+    documents: ['/documents/joker_test.jpeg'],
     due_date: Date.today,
-    reminder_date_time:, DateTime.now + 30.minutes,
-    user: alexis
-    },
+    reminder_datetime: DateTime.now + 30.minutes,
+    user: alexis },
 
-  { title: ''
+  { title: 'Printing documents',
+    description: 'Check and print supporting documents for claims',
+    priority: 'high',
+    documents: ['/documents/joker_test.jpeg'],
+    due_date: Date.today,
+    reminder_datetime: DateTime.now + 60.minutes,
+    user: alexis },
 
-  }
+  { title: 'Signing and stamping',
+    description: 'Get printed documents to head for authorisation',
+    priority: 'high',
+    due_date: Date.today,
+    reminder_datetime: DateTime.now + 120.minutes,
+    user: alexis },
 
+  { title: 'Archiving',
+    description: 'File documents and pass to archives',
+    priority: 'low',
+    due_date: Date.new(2024, 05, 31),
+    reminder_datetime: DateTime.new(2024, 05, 30, 18, 00, 00),
+    user: alexis },
 
-
-
-
-
-
-
+  { title: 'Prepare inter-co reconciliations',
+    description: 'Identify transactions, double check for duplicates and prepare journal entries',
+    priority: 'medium',
+    due_date: Date.new(2024, 05, 20),
+    reminder_datetime: DateTime.new(2024, 05, 19, 15, 30, 00),
+    user: alexis }
 ]
+
+new_tasks.each do |attributes|
+  task = Task.create!(attributes)
+
+  puts "Created #{task.title}"
+end
