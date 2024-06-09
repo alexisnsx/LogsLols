9e0667300689eda5b22755276d62d2fe8af07c20
Karen Ann <kamxlee@gmail.com>
2024-06-09 21:07:20 +0800

    Refactored streaming

--- app/services/chat_service.rb
+++ app/services/chat_service.rb
@@ -11,7 +11,7 @@ class ChatService
     # In rails c, test with "ChatService::OpenAiCompletion.new(prompt: "I need something to cheer me up").call"
 
     def call
-      messages = persona_instructions.map do |instruction|
+      messages = system_instructions.map do |instruction|
         { role: "system", content: instruction}
       end
 
@@ -28,7 +28,7 @@ class ChatService
 
     private
 
-    def persona_instructions
+    def system_instructions
       [
         "You are a famous chef that loves to share simple, easy-to-cook recipes."
       ]
@@ -50,22 +50,7 @@ class ChatService
     end
 
     def call
-      sse = SSE.new(@response.stream, event: "message")
-      begin
-        client.chat(
-          parameters: {
-            model: "gpt-3.5-turbo-1106",
-            messages: [{ role: "user", content: @prompt }],
-            stream: proc do |chunk|
-              content = chunk.dig('choices', 0, 'delta', 'content')
-              return if content.nil?
-              sse.write({ message: content })
-            end
-          }
-        )
-      ensure
-        sse.close
-      end
+      stream_response(@response, @prompt)
     end
 
     private
@@ -86,22 +71,7 @@ class ChatService
     end
 
     def call
-      sse = SSE.new(@response.stream, event: "message")
-      begin
-        client.chat(
-          parameters: {
-            model: "llama3-70b-8192",
-            messages: [{ role: "user", content: @prompt }],
-            stream: proc do |chunk|
-              content = chunk.dig('choices', 0, 'delta', 'content')
-              return if content.nil?
-              sse.write({ message: content })
-            end
-          }
-        )
-      ensure
-        sse.close
-      end
+      stream_response(@response, @prompt)
     end
 
     private
@@ -115,4 +85,82 @@ class ChatService
     end
   end
 
+  class GroqToolUseStream < ChatService
+    def initialize(prompt:, response:)
+      @prompt = prompt
+      @response = response
+    end
+
+    def call
+      messages = system_instructions.map do |instruction|
+        { role: "system", content: instruction}
+      end
+
+      messages << { role: "user", content: prompt}
+
+      stream_tool_response(@response, messages)
+    end
+
+    private
+
+    def system_instructions
+      [
+        "You are a function-calling LLM that knows how to use functions stored in the tools array to get answers for the user."
+      ]
+    end
+
+    def get_weather(location, unit = 'celsius')
+      "Weather is fine!"
+    end
+
+    WEATHER_TOOL = {
+      type: "function",
+      function: {
+        name: "get_weather",
+        description: "Get the current weather in a given location",
+        parameters: {
+          type: :object,
+          properties: {
+            location: {
+              type: :string,
+              description: "The city and state, e.g. San Francisco, CA",
+            },
+            unit: {
+              type: "string",
+              enum: %w[celsius fahrenheit],
+            },
+          },
+          required: ["location"],
+        },
+      },
+    }
+
+
+  def stream_tool_response(response, messages)
+    sse = SSE.new(response.stream, event: "message")
+    begin
+      client.chat(
+        parameters: {
+          model: "llama3-70b-8192",
+          messages: [{ role: "user", content: prompt }],
+          stream: proc do |chunk|
+            content = chunk.dig('choices', 0, 'delta', 'content')
+            return if content.nil?
+            sse.write({ message: content })
+          end
+        }
+      )
+    ensure
+      sse.close
+    end
+  end
+
+  def client
+    @_client ||= OpenAI::Client.new(
+      access_token: ENV.fetch('GROQ_ACCESS_TOKEN'),
+      uri_base: "https://api.groq.com/openai",
+      log_errors: true
+    )
+  end
+  end
 end
